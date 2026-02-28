53d31e71554c806d980bb32f8b4a3aec97c1118a
taijulsir <princetaijul123@gmail.com>
2026-02-28 23:29:26 +0600

    chore: update config and dependencies for local assets fetching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5001',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '5001',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      }
+    ],
+    // Removed localPatterns to allow default Next.js behavior for images in the /public folder to work properly
+  },
+  experimental: {}
 };
 
 export default nextConfig;
